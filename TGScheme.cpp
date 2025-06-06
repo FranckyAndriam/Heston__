4776046d203507e1a6ab5615b568ed1c1526527a
Francky Andriamampionona <andriafrancky3@gmail.com>
2025-06-06 21:37:19 +0200

    Add files via upload

--- TGScheme.cpp
+++ TGScheme.cpp
@@ -1,74 +1,132 @@
-#define _USE_MATH_DEFINES
+﻿#define _USE_MATH_DEFINES
 #include "TGScheme.h"
 #include <cmath>
 #include <algorithm>
 
-TGScheme::TGScheme(double kappa_, double theta_, double epsilon_, double dt_)
-    : kappa(kappa_), theta(theta_), epsilon(epsilon_), dt(dt_), gen(std::random_device{}()), norm(0.0, 1.0) {}
+TGScheme::TGScheme(double psi_min, double psi_max, int grid_size)
+    : psi_min_(psi_min), psi_max_(psi_max), grid_size_(grid_size) {
+    initialize_grid();
+}
+
+TGScheme::TGScheme(const TGScheme& other)
+    : psi_min_(other.psi_min_), psi_max_(other.psi_max_), grid_size_(other.grid_size_),
+    psi_grid_(other.psi_grid_), r_grid_(other.r_grid_) {}
 
 TGScheme& TGScheme::operator=(const TGScheme& other) {
     if (this != &other) {
-        kappa = other.kappa;
-        theta = other.theta;
-        epsilon = other.epsilon;
-        dt = other.dt;
-        gen = other.gen;
-        norm = other.norm;
+        psi_min_ = other.psi_min_;
+        psi_max_ = other.psi_max_;
+        grid_size_ = other.grid_size_;
+        psi_grid_ = other.psi_grid_;
+        r_grid_ = other.r_grid_;
     }
     return *this;
 }
 
-TGScheme::~TGScheme() {}
+double TGScheme::phi(double x) {
+    return std::exp(-0.5 * x * x) / std::sqrt(2 * M_PI);
+}
 
-double TGScheme::step(
-    const double& Vt,
-    const double& /*dt_in*/,
-    const double& /*kappa_in*/,
-    const double& /*theta_in*/,
-    const double& /*epsilon_in*/,
-    std::mt19937& /*rng*/
-) const {
-    return simulateNextV(Vt);
+double TGScheme::Phi(double x) {
+    return 0.5 * std::erfc(-x / std::sqrt(2));
+}
+
+double TGScheme::objective_function(double r, double psi) {
+    double ph = phi(r);
+    double PH = Phi(r);
+    return r * ph + (1 + r * r) * PH - (1 + psi) * std::pow(ph + r * PH, 2);
+}
+
+double TGScheme::objective_derivative(double r, double psi) {
+    double ph = phi(r);
+    double PH = Phi(r);
+    double common_term = ph + r * PH;
+
+    double derivative_common_term = -r * ph + PH;
+
+    double derivative = ph + 2 * r * PH -
+        2 * (1 + psi) * common_term * derivative_common_term;
+
+    return derivative;
+}
+
+double TGScheme::compute_r(double psi) const {
+    // Méthode de Newton robuste (selon Andersen)
+    double r = (psi < 1.0) ? 1.0 : -4.0; // Initial guess selon Andersen
+    const int max_iter = 100;
+    const double tol = 1e-10;
+
+    for (int iter = 0; iter < max_iter; ++iter) {
+        double f = objective_function(r, psi);
+        double df = objective_derivative(r, psi);
+
+        if (std::abs(df) < 1e-8) df = (df < 0 ? -1 : 1) * 1e-8; // Protection contre division zéro
+
+        double delta = f / df;
+        r -= delta;
+
+        if (std::abs(delta) < tol) break;
+
+        // Clamping pour stabilité
+        r = std::min(std::max(r, -10.0), 10.0);
+    }
+
+    return r;
+}
+
+void TGScheme::initialize_grid() {
+    psi_grid_.resize(grid_size_);
+    r_grid_.resize(grid_size_);
+
+    double dpsi = (psi_max_ - psi_min_) / (grid_size_ - 1);
+
+    for (int i = 0; i < grid_size_; ++i) {
+        double psi = psi_min_ + i * dpsi;
+        psi_grid_[i] = psi;
+        r_grid_[i] = compute_r(psi);
+    }
+}
+
+double TGScheme::step(const double& Vt,
+    const double& dt,
+    const double& kappa,
+    const double& theta,
+    const double& epsilon,
+    std::mt19937& rng) const {
+    double exp_kdt = std::exp(-kappa * dt);
+    double m = theta + (Vt - theta) * exp_kdt;
+
+    double s2 = Vt * epsilon * epsilon * exp_kdt * (1.0 - exp_kdt) / kappa +
+        theta * epsilon * epsilon * std::pow(1.0 - exp_kdt, 2) / (2.0 * kappa);
+
+    double psi = s2 / (m * m);
+
+    // Interpolation linéaire de r
+    double r;
+    if (psi <= psi_min_) {
+        r = r_grid_.front();
+    }
+    else if (psi >= psi_max_) {
+        r = r_grid_.back();
+    }
+    else {
+        double idx = (psi - psi_min_) / (psi_max_ - psi_min_) * (grid_size_ - 1);
+        int idx_lower = static_cast<int>(idx);
+        double weight = idx - idx_lower;
+        r = r_grid_[idx_lower] * (1.0 - weight) + r_grid_[idx_lower + 1] * weight;
+    }
+
+    double ph = phi(r);
+    double PH = Phi(r);
+    double mu = (m * r) / (ph + r * PH);
+    double sigma = m / (ph + r * PH);
+
+    std::normal_distribution<double> normal(0.0, 1.0);
+    double Z = normal(rng);
+
+    return std::max(0.0, mu + sigma * Z);
 }
 
 TGScheme* TGScheme::clone() const {
     return new TGScheme(*this);
-}
-
-double TGScheme::simulateNextV(double Vt) const {
-    double m = computeMean(Vt);
-    double s2 = computeVariance(Vt);
-    double psi = computePsi(m, s2);
-    double r = lookup_r(psi);
-
-    double phi_r = std::exp(-0.5 * r * r) / std::sqrt(2.0 * M_PI);
-    double Phi_r = 0.5 * (1.0 + std::erf(r / std::sqrt(2.0)));
-
-    double f_mu = r / (phi_r + r * Phi_r);
-    double f_sigma = std::sqrt(psi) * f_mu;
-
-    double mu = f_mu * m;
-    double sigma = f_sigma * std::sqrt(s2);
-
-    double Z = norm(gen);
-    return std::max(0.0, mu + sigma * Z);
-}
-
-double TGScheme::computeMean(double Vt) const {
-    return theta + (Vt - theta) * std::exp(-kappa * dt);
-}
-
-double TGScheme::computeVariance(double Vt) const {
-    double ekt = std::exp(-kappa * dt);
-    double term1 = Vt * epsilon * epsilon * ekt * (1.0 - ekt) / kappa;
-    double term2 = theta * epsilon * epsilon * std::pow(1.0 - ekt, 2) / (2.0 * kappa);
-    return term1 + term2;
-}
-
-double TGScheme::computePsi(double m, double s2) const {
-    return s2 / (m * m);
-}
-
-double TGScheme::lookup_r(double psi) const {
-    return std::sqrt(2.0 * std::log(1.0 + psi));
 }
--- TGScheme.h
+++ TGScheme.h
@@ -1,39 +1,42 @@
-#ifndef TGSCHEME_H
+﻿#ifndef TGSCHEME_H
 #define TGSCHEME_H
 
 #include "Variance.h"
+#include <vector>
 #include <random>
+#include <mutex>
 
 class TGScheme : public Variance {
 public:
-    TGScheme(double kappa_, double theta_, double epsilon_, double dt_);
-    TGScheme(const TGScheme& other) = default;
+    TGScheme(double psi_min = 0.001, double psi_max = 10.0, int grid_size = 5000);
+    TGScheme(const TGScheme& other);
     TGScheme& operator=(const TGScheme& other);
-    ~TGScheme() override;
+    ~TGScheme() override = default;
 
-    double step(
-        const double& Vt,
+    double step(const double& Vt,
         const double& dt,
         const double& kappa,
         const double& theta,
         const double& epsilon,
-        std::mt19937& rng
-    ) const override;
+        std::mt19937& rng) const override;
 
     TGScheme* clone() const override;
 
 private:
-    double simulateNextV(double Vt) const;
-    double computeMean(double Vt) const;
-    double computeVariance(double Vt) const;
-    double computePsi(double m, double s2) const;
-    double lookup_r(double psi) const;
+    double psi_min_, psi_max_;
+    int grid_size_;
 
-    mutable std::mt19937 gen;
-    mutable std::normal_distribution<double> norm;
+    std::vector<double> psi_grid_;
+    std::vector<double> r_grid_;
 
-    double kappa, theta, epsilon, dt;
+    void initialize_grid();
+    double compute_r(double psi) const;
+    static double objective_function(double r, double psi);
+    static double objective_derivative(double r, double psi);
+    static double phi(double x);
+    static double Phi(double x);
+
+    mutable std::mutex cache_mutex_;
 };
 
 #endif // TGSCHEME_H
-
